b71661d2c96281c4565615fa9941962fda5bdd67
Muhammad Abdullah Abid <abd@abd.dev>
2023-09-15 14:24:54 -0700

    fix(site.config.ts): update rootNotionPageId to '16ccc94eb4cf4b3d85fb31ac7be58e87' to point to the correct Notion page

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'd3abf13ed15247cb92630209c99742d7',
+  rootNotionPageId: '16ccc94eb4cf4b3d85fb31ac7be58e87',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
